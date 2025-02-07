bb0242fa7f141318d9fb75f2c6d91247f72b6a32
desa-thomas <desa2thomas@gmail.com>
2025-02-06 19:52:25 -0500

    Client CLI improvements

--- client.py
+++ client.py
@@ -1,18 +1,31 @@
 import socket
+import os
 
 def start_client():
+    #clear CLI screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{"CP372 Programming Assignment Client Interface":-^100}")
+    
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 1738))  # Connect to the server
 
+    #First message cilent sends name
+    message = input("\n(Enter name)--> ")
+    client_socket.send(message.encode())
+    data = client_socket.recv(1024).decode()
+    print(f"{data}\n")
+
+    
+    #Main loop, clien can send any string
     while True:
-        message = input("Enter name: ")
-        if message == "quit" or message == "q":
+        message = input("--> ")
+        if message.lower() == "quit" or message.lower() == "q":
             break
         
         client_socket.send(message.encode())
 
         data = client_socket.recv(1024).decode()
-        print(f"Received from server: {data}")
+        print(f"{data}\n")
 
     client_socket.close()
 
--- server.py
+++ server.py
@@ -3,9 +3,18 @@ import socket
 from datetime import datetime
 
 class server:
+    """
+    Server class
+    
+    attributes:
+        self.count : (int) number of clients that have connected (since server start)
+        self.client_name_dict : (dict) dictionary containing information about connected clients
+        self.server_socket : (socket) server socket
+    """
     MAX_CLIENTS = 3
     
     def __init__(self):
+        
         #initialize server socket
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('localhost', 1738))
@@ -17,36 +26,60 @@ class server:
     def handle_client(self, client_socket: socket.socket, address, i):
         """
         Function to handle client i
-        """
-        print(f"{address} connected @: {datetime.now()}")
-        
+        """        
         #First message sent from client is name
         name = client_socket.recv(1024).decode()
         if name:
-            print(f"Client{i} - {name}")
+            print(f"Client{i}: {name} connected")
             client_socket.send(bytes(f"Welcome {name}", 'utf-8'))
             
-            #Store clients name
-            self.client_name_dict[f"Client{i}"].insert(0, name) 
+            #Store client info
+            self.client_name_dict[f"Client{i}"] = []
+            self.client_name_dict[f"Client{i}"].append(name)
+            self.client_name_dict[f"Client{i}"].append(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+
         
         #Maintain connection and echo {data} ACK while they are connected
         while True:
             
             data = client_socket.recv(1024).decode()
+            
             #If client closes connection
             if not data:
                 break
             
-            #Echo message back with ACK
-            client_socket.send(bytes(f"{data} ACK", "utf-8"))
-            
+            #Send status if requested
+            if(data.lower() == "status" ):
+                status = "\n" + "-"*100 + "\n"
+                status += f"{"Client id":<10}{"Name":^30}{"Start time":^30}{"End time":^30}"
+                status += "\n\n"
+                
+                #iterate ovfer dict
+                for client in self.client_name_dict.items():
+                    status += f"{client[0]:<10}"
+                    #iterate over info array
+                    for clientinfo in client[1]:
+                        status += f"{clientinfo:^30}"
+                    
+                    status += "\n"
+                    
+                status += "-"*100 + "\n"
+                
+                client_socket.send(bytes(status, "utf-8"))
+            else: 
+                #Echo message back with ACK
+                client_socket.send(bytes(f"{data} ACK", "utf-8"))
+                
         #store end time
-        self.client_name_dict[f"Client{i}"].append(datetime.now().timestamp())
-        print(f"Connection closed: {address}")
-        print(f"Client{i} : {self.client_name_dict[f"Client{i}"]}")
+        self.client_name_dict[f"Client{i}"].append(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        print(f"{name} quit: {address}")
         client_socket.close()
 
+
     def start_server(self):
+        """
+        Function to start the server
+        """
         self.server_socket.listen(server.MAX_CLIENTS)
         print("Server is listening")
         
@@ -54,9 +87,7 @@ class server:
             client_socket, addr = self.server_socket.accept()
             #i = client #
             i = self.count
-            self.count += 1 
-            self.client_name_dict[f"Client{i}"] = [datetime.now().timestamp()]
-            
+            self.count += 1             
             #create thread to handle client # i
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket, addr, i))
             client_thread.daemon = True
